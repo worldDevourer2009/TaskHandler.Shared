98a1dff75d62f79e1fc3f212f1cca4766594f4a8
worldDevourer <danyterx123@gmail.com>
2025-07-02 15:08:35 +0200

    updates for kafka

---
+++ Kafka/EventTypes/AuthService/AuthEventTypes.cs
@@ -0,0 +1,8 @@
+namespace TaskHandler.Shared.Kafka.EventTypes.AuthService;
+
+public static class AuthEventTypes
+{
+    public static string UserLoggedIn = "user-logged-in";
+    public static string UserSignedUp = "user-signed-up";
+    public static string UserLoggedOut = "user-logged-out";
+}
